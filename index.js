77167992419231ff39f3804b0a9da2944788c4de
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2024-04-22 10:11:54 +0300

    Fix rate limiting logic in /send-mail endpoint

--- index.js
+++ index.js
@@ -18,18 +18,25 @@ app.get("/", (req, res) => {
 });
 
 const timeout = new Map();
-
+const tenMinutes = 1000 * 60 * 10;
 app.get("/send-mail", async (req, res) => {
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-  if (timeout.get(ip)) {
-    return res.status(400).send({ message: "Timeout" });
+  if (timeout.has(ip)) {
+    const t = timeout.get(ip);
+    const diff = Date.now() - t;
+    const minutes = Math.floor((tenMinutes - diff) / 60000);
+    const seconds = Math.floor((tenMinutes - diff) / 1000) % 60;
+
+    return res.status(400).send({
+      message: `You can only send an email every 10 minutes. Please wait ${minutes} minutes and ${seconds} seconds.`,
+    });
   }
 
-  timeout.set(ip, true);
+  timeout.set(ip, Date.now());
   setTimeout(() => {
     timeout.delete(ip);
-  }, Math.random() * 1000 * 60 * 10);
+  }, Math.random() * tenMinutes);
 
   await sendMail(JSON.stringify(req.headers, null, 2));
 
